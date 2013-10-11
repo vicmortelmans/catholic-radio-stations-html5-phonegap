3b904774b64fd9ff23707be2aa2018d38d238ae9
OpenShift guest <vic.mortelmans@telenet.be>
2013-10-11 11:18:50 -0400

    adding radio stations

--- radio.js
+++ radio.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var getijdenrooster = [];
-    var getijdenroosterurl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.rkk.nl%2Fscripts%2Fgetijden%2Fstream-embed.php%22&format=json&callback=p";
+    var getijdenroosterurl = "http://pipes.yahoo.com/pipes/pipe.run?_id=65561586a6f6bf908b7a91e02d0e6f33&_render=json&_callback=p";
     var getijdenroosterladen = $.getJSON(getijdenroosterurl);
     getijdenroosterladen.done(function(d){
         $.each(d['query']['results']['tr'],function(tr){
