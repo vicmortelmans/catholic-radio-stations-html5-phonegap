8c2a72cb313e7794c13af5752b09a6307aa78145
OpenShift guest <vic.mortelmans@telenet.be>
2013-10-11 16:02:46 -0400

    adding radio stations

--- radio.js
+++ radio.js
@@ -1,5 +1,6 @@
+var getijdenrooster = [];
+
 $(document).ready(function(){
-    var getijdenrooster = [];
     var getijdenroosterurl = "http://pipes.yahoo.com/pipes/pipe.run?_id=65561586a6f6bf908b7a91e02d0e6f33&_render=json&_callback=?";
     var getijdenroosterladen = $.getJSON(getijdenroosterurl);
     getijdenroosterladen.done(function(d){
